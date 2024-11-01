644690679a607e33b9727d9cc998effd619aa400
Haonan Ye <haonan.ye@edu.sait.ca>
2024-10-31 23:44:20 -0600

    Update App.js and ToDoList.js

--- components/ToDoList.js
+++ components/ToDoList.js
@@ -1,31 +1,16 @@
-import React from 'react';
-import {
-  StyleSheet,
-  Pressable,
-  View,
-  Text,
-  ScrollView,
+import React from "react";
+import { StyleSheet, Pressable, View, Text, ScrollView } from "react-native";
 
-} from 'react-native';
-
-const ToDoList = () => {
+const ToDoList = ({ tasks }) => {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {tasks.map((task, index) => (
+        <Pressable key={index}>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        </Pressable>
+      ))}
     </ScrollView>
   );
 };
@@ -34,10 +19,10 @@ const styles = StyleSheet.create({
   task: {
     padding: 10,
     borderBottomWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
   },
   completed: {
-    backgroundColor: '#e0e0e0',
+    backgroundColor: "#e0e0e0",
   },
   taskText: {
     fontSize: 16,
